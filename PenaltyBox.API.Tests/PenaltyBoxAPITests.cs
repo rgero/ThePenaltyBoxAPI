edc7312874a4afc476c9645bd27f1539520d8dc9
Roy Gero <roy.gero.ii@gmail.com>
2022-07-16 19:16:17 -0400

    Adding tests to exercise the delete functionality

--- PenaltyBox.API.Tests/PenaltyBoxAPITests.cs
+++ PenaltyBox.API.Tests/PenaltyBoxAPITests.cs
@@ -13,33 +13,36 @@ namespace PenaltyBox.API.Tests
 {
     public class PenaltyBoxAPITests
     {
-        PenaltiesController controller;
-        PenaltyContext context;
-        DbContextOptions<PenaltyContext> options;
+        readonly PenaltiesController controller;
+        readonly PenaltyContext context;
+        readonly DbContextOptions<PenaltyContext> options;
 
         public PenaltyBoxAPITests()
         {
             options = new DbContextOptionsBuilder<PenaltyContext>().UseInMemoryDatabase("TestingDatabase").Options;
             context = new PenaltyContext(options);
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
             controller = new PenaltiesController(context);
         }
 
         [Fact]
-        public void GetNoPenaltiesTest ()
+        public async void GetNoPenaltiesTest ()
         {
-            context.Database.EnsureDeletedAsync();
-            var result = controller.GetPenalties();
-            var task = Assert.IsType<Task<ActionResult<IEnumerable<Penalty>>>>(result);
-            var actionResult= Assert.IsType<ActionResult<IEnumerable<Penalty>>>(task.Result);
+            var result = await controller.GetPenalties();
+            var actionResult= Assert.IsType<ActionResult<IEnumerable<Penalty>>>(result);
 
             List<Penalty> penaltyList = Assert.IsType<List<Penalty>>(actionResult.Value);
-            Assert.Equal(0, penaltyList.Count());
+            Assert.Equal(0, penaltyList.Count);
         }
 
         [Fact]
-        public void PostValidPenaltyTest ()
+        public async void GetsNumberOfPenaltiesTest()
         {
-            Penalty testPenalty = new Penalty()
+            // Set Up - Add 2 valid penalties
+            Penalty testPenalty = new()
             {
                 Player = "Patrice",
                 Team = "Bruins",
@@ -50,9 +53,45 @@ namespace PenaltyBox.API.Tests
                 Referees = new string[] { "Steve", "Bob" }
             };
 
-            var result = controller.PostPenalty(testPenalty);
-            var task = Assert.IsType<Task<ActionResult<Penalty>>>(result);
-            var actionResult = Assert.IsType<ActionResult<Penalty>>(task.Result);
+            Penalty testPenalty2 = new()
+            {
+                Player = "Shamed",
+                Team = "Avalanche",
+                Opponent = "Islanders",
+                GameDate = new System.DateTime(),
+                Home = false,
+                PenaltyName = "Hooking",
+                Referees = new string[] { "Steve", "Bob" }
+            };
+
+            List<Penalty> testPenalties = new() { testPenalty, testPenalty2 };
+
+            context.AddRange(testPenalties);
+            await context.SaveChangesAsync();
+
+            var result = await controller.GetPenalties();
+            var actionResult = Assert.IsType<ActionResult<IEnumerable<Penalty>>>(result);
+
+            List<Penalty> penaltyList = Assert.IsType<List<Penalty>>(actionResult.Value);
+            Assert.Equal(2, penaltyList.Count);
+        }
+
+        [Fact]
+        public async void PostValidPenaltyTest ()
+        {
+            Penalty testPenalty = new()
+            {
+                Player = "Patrice",
+                Team = "Bruins",
+                Opponent = "Islanders",
+                GameDate = new System.DateTime(),
+                Home = false,
+                PenaltyName = "Hooking",
+                Referees = new string[] { "Steve", "Bob" }
+            };
+
+            var result = await controller.PostPenalty(testPenalty);
+            var actionResult = Assert.IsType<ActionResult<Penalty>>(result);
             var createdResult = Assert.IsType<CreatedAtActionResult>(actionResult.Result);
             Penalty returnedPenalty = Assert.IsType<Penalty>(createdResult.Value);
 
@@ -68,7 +107,7 @@ namespace PenaltyBox.API.Tests
         [Fact]
         public void PostPenalty_Should_Throw_When_Not_Valid_Penalty()
         {
-            Penalty testPenalty = new Penalty()
+            Penalty testPenalty = new()
             {
                 Player = "Patrice",
                 Team = "Bruins",
@@ -78,15 +117,51 @@ namespace PenaltyBox.API.Tests
                 Referees = new string[] { "Steve", "Bob" }
             };
 
-            controller.ModelState.AddModelError("PenaltyNameMissing", "No Penalty name defined.");
-            var result = controller.PostPenalty(testPenalty);
-            var task = Assert.IsType<Task<ActionResult<Penalty>>>(result);
-
-            var exception = Record.Exception(() => task.Result);
+            var exception = Record.Exception(() => controller.PostPenalty(testPenalty).Result);
 
             Assert.NotNull(exception);
             Assert.IsType<AggregateException>(exception);
         }
 
+        [Fact]
+        public async void DeletePenaltyTest()
+        {
+            Penalty testPenalty = new()
+            {
+                Player = "Patrice",
+                Team = "Bruins",
+                Opponent = "Islanders",
+                GameDate = new System.DateTime(),
+                Home = false,
+                PenaltyName = "Hooking",
+                Referees = new string[] { "Steve", "Bob" }
+            };
+
+            context.Add(testPenalty);
+            await context.SaveChangesAsync();
+
+            List<Penalty> penaltyList = await context.Penalties.ToListAsync<Penalty>();
+            Assert.Equal(1, penaltyList.Count);
+
+            int targetID = 1;
+            var result = await controller.DeletePenalty(targetID);
+
+            Assert.NotNull(result);
+            Assert.IsType<NoContentResult>(result);
+
+            penaltyList = await context.Penalties.ToListAsync<Penalty>();
+            Assert.Equal(0, penaltyList.Count);
+        }
+
+        [Fact]
+        public async void NoDeleteInvalidPenaltyTest()
+        {
+            int targetID = 2;
+            var result = await controller.DeletePenalty(targetID);
+
+            Assert.NotNull(result);
+            Assert.IsType<NotFoundResult>(result);
+
+        }
     }
 }
